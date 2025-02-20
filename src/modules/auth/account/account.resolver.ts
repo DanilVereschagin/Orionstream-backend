b81685c2cfb017d9d1ca1f3bafd50855ecab0e69
Danil Vereshchagin <diafin.bernuly@mail.ru>
2025-02-20 21:12:37 +0300

    Add api for registration

--- src/modules/auth/account/account.resolver.ts
+++ src/modules/auth/account/account.resolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AccountService } from './account.service';
+import { CreateUserInput } from './inputs/create-user.input';
 import { UserModel } from './models/user.model';
 
 @Resolver('Account')
@@ -9,5 +10,10 @@ export class AccountResolver {
 	@Query(() => [UserModel], { name: 'findAllUsers' })
 	public async findAll() {
 		return this.accountService.findAll();
+	}
+
+	@Mutation(() => Boolean, { name: 'createUser' })
+	public async create(@Args('data') input: CreateUserInput) {
+		return this.accountService.create(input);
 	}
 }
--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
 import { PrismaService } from '@/src/core/prisma/prisma.service';
+import { CreateUserInput } from './inputs/create-user.input';
 
 @Injectable()
 export class AccountService {
@@ -8,5 +10,40 @@ export class AccountService {
 	public async findAll() {
 		const users = await this.prismaService.user.findMany();
 		return users;
+	}
+
+	public async create(input: CreateUserInput) {
+		const { email, password, username } = input;
+
+		const isUsernameExists = await this.prismaService.user.findUnique({
+			where: {
+				username,
+			},
+		});
+
+		if (isUsernameExists) {
+			throw new ConflictException('Username already exists');
+		}
+
+		const isEmailExists = await this.prismaService.user.findUnique({
+			where: {
+				email,
+			},
+		});
+
+		if (isEmailExists) {
+			throw new ConflictException('Email already exists');
+		}
+
+		await this.prismaService.user.create({
+			data: {
+				email,
+				password: await hash(password),
+				username,
+				displayName: username,
+			},
+		});
+
+		return true;
 	}
 }
